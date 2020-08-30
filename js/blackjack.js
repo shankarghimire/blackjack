f6129e9f50fbf1386fc377a03759dc6d8754b6ee
Shankar Ghimire <ssghimire@gmail.com>
2020-08-30 19:49:06 -0400

    Adds methods that deals 2, 2 cards to dealer and player

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,38 +1,56 @@
- //var to hold
+ //globar variables to hold cards information
  var suits = ["spades","hearts","clubs","diams"];
  var numbers = ["A","2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
- var deckOfCards = [];
+ var deckOfCards=[]; //an empty array to hold 52 cards
+ var cardCount = 0; //
+ var playerCard=[];
+ var dealerCard=[];
 
  
  //variables to hold DOMs
  var output;
- var text;
+ var message;
+ var btnStart;
+ var start;
+ var dealerHand;
+ var playerHand;
+
 
 
  //Adds event listener
  window.addEventListener('load',init);
+ btnStart = document.addEventListener('click', startGame,false); 
 
  function init(){
+     //initiales global variables
+     suits = ["spades","hearts","clubs","diams"];
+     numbers ;
+     deckOfCards = []; //an empty array to hold 52 card objects
+     cardCount = 0;  //to count the top card to deal
+     playerCard = [];
+     dealerCard = [];
+
      //initializes all DOMs to varibles
      output = document.getElementById('output');
-     test = document.getElementById('test');
+     message = document.getElementById('message');
+     btnStart = document.getElementById('btnStart');
+     start = document.getElementById('start');
+     dealerHand = document.getElementById('dealerHand');
+     playerHand = document.getElementById('playerHand');
      
-     //calls function
-     createDeckOfCards();
-     
+     //section to call other functions
+     createDeckOfCards();   
+  
+    console.log("After windows loads events: ");
+    console.log(deckOfCards);
  }
 
  function createDeckOfCards(){
      for(s in suits){
-         var suit = suits[s][0].toUpperCase();
-         var bgcolor = (suit == 'S' || suit == 'C') ? "#000000" : "#ff0000";
-
+         let suit = suits[s][0].toUpperCase();
+         let bgcolor = (suit == 'S' || suit == 'C') ? "#000000" : "#ff0000";
          for(n in numbers){
-             //output.innerHTML += "<span style=color: bgcolor> </span> ";  numbers[n]+ "&" +  suits[s] +"; "  ;
-             // output.innerHTML += "<span style='color:" + bgcolor +"';>" +  numbers[n] + "&" + suits[s]+ ";</span> ";
-
              let cardValue =( ( n > 9 ) ? 10 : (parseInt(n) + 1) );
-
              //creates a Card Object
              let aCard = {
                  suit: suit,
@@ -45,12 +63,60 @@
              //console.log(numbers[n]+ "&" +  suits[s]  + ";");
          }   
      }
-     console.log(deckOfCards);  
-     randomCard();     
+     //console.log(deckOfCards);  
+     //randomCard();     
  }
 
  function randomCard(){
      let randomNum = Math.floor(Math.random() * 52 );
      output.innerHTML += "<span style='color:" + deckOfCards[randomNum].bgcolor +"';> &" +  deckOfCards[randomNum].icon + ";" + deckOfCards[randomNum].cardNum+ "</span> ";
+     //output.innerHTML += "<span style='color:" + deckOfCards[0].bgcolor +"';> &" +  deckOfCards[0].icon + ";" + deckOfCards[0].cardNum+ "</span> ";
  }
 
+ //function to shuffle the deck of cards
+function  shuffleDeckOfCards(cardArray){
+    for(let i = (cardArray.length-1); i >=0; i--){
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = cardArray[i];
+        cardArray [i] = cardArray[j];
+        cardArray[j] = temp;
+    }
+    return cardArray;
+}
+
+
+function startGame(){
+    //alert("Testing click event from start button");
+   shuffleDeckOfCards(deckOfCards);
+
+   //function call to deal cards
+   dealNewCards();
+   console.log("After start button is clicked: ");
+   console.log(deckOfCards);
+   //randomCard();
+}
+
+function dealNewCards(){
+    playerCard = [];
+    dealerCard = [];
+    dealerHand.innerHTML = "";
+    playerHand.innerHTML = "";
+
+    let x;
+    for(x = 0; x < 2; x++){
+        dealerCard.push(deckOfCards[cardCount]);
+        dealerHand.innerHTML += cardOutput();
+        cardCount++;
+        playerCard.push(deckOfCards[cardCount]);
+        playerHand.innerHTML += cardOutput();
+        cardCount++;
+    }
+    console.log("Cards at Dealer's Hand : ");
+    console.log(dealerCard);
+    console.log("Cards at Player's Hand : ")
+    console.log(playerCard);
+
+}
+ function cardOutput(){
+    return "<span style='color:" + deckOfCards[cardCount].bgcolor + "';>" + deckOfCards[cardCount].cardNum + "&" + deckOfCards[cardCount].icon + ";</span>";
+ }
